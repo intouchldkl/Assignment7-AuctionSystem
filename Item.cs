eab7cde64821d983a776957ad08bf45c57bd2f2d
ADMINS <ADMINS@DESKTOP-QFR4487>
2021-01-25 12:24:46 +0700

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment7_AuctionSystem
+{
+    class Item
+    {
+        private string description;
+        private double reservePrice;
+        private double highestBid;
+        private int bidderIDNumber;
+
+
+         public Item(string description,double reservePrice)
+        {
+            this.description = description;
+            this.reservePrice = reservePrice;
+            highestBid = 0;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+        public double getReservePrice()
+        {
+            return reservePrice;
+        }
+        public double getHighestBid()
+        {
+            return highestBid;
+        }
+        public int getBidderId()
+        {
+            return bidderIDNumber;
+        }
+
+        public bool setHighestBid(double newBid)
+        {
+            if (newBid > highestBid)
+            {
+                highestBid = newBid;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public void setBidderID(int newID)
+        {
+            bidderIDNumber = newID;
+        }
+
+        public string GetInfo()
+        {
+            return description + " " + highestBid + " " + reservePrice;
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Assignment7_AuctionSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] items = new Item[5];
+            items[0] = new Item("Faberge", 10000000);
+            items[1] = new Item("Rolex Submarine", 300000);
+            items[2] = new Item("Louis XII whiskey", 80000);
+            items[3] = new Item("Intouch's wine glass", 99000);
+            items[4] = new Item("Mr.H's security software", 1000);
+        }
+
+           static void printAllItems(Item[] items)
+            {
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i] != null)
+                    {
+                        Console.WriteLine(items[i].GetInfo());
+                    }
+                }
+            }
+        static void BidOnItem(int itemNumber,double bid,int bidderIDNumber,Item[] items)
+        {
+            if(bid > items[itemNumber].getHighestBid)
+        }
+
+            
+
+    }
+}
